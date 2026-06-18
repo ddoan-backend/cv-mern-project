3c49fcea66e219a97d831bd4c2f512cf0ba51ec2
DoanBackendn <ddoan04030l@gmail.com>
2026-06-19 01:17:34 +0700

    fix layout

--- frontend/src/pages/SigninPage.jsx
+++ frontend/src/pages/SigninPage.jsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/login-form"
 
 function SigninPage() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-amber-200">
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
